cf0569fc77a50bb24940ed8391786cfa126e745a
Simon Márton <simon.marton@bitrise.io>
2020-08-05 20:11:24 +0200

    chore: getChangedSteps utility method

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,4 +1,14 @@
-module.exports.convertSpecToRecords = (stepList) =>
+const zipObject = require('lodash.zipobject');
+const get = require('lodash.get');
+const isEqual = require('lodash.isequal');
+
+const pick = (obj, paths) =>
+  zipObject(
+    paths,
+    paths.map((key) => get(obj, key))
+  );
+
+const convertSpecToRecords = (stepList) =>
   Object.entries(stepList).reduce(
     ({ steps, inputs }, [id, { versions, ...details }]) => {
       const { steps: s, inputs: i } = Object.entries(versions).reduce(
@@ -37,5 +47,25 @@ module.exports.convertSpecToRecords = (stepList) =>
     },
     { inputs: [], steps: [] }
   );
+const differenceBy = (array1, array2, key) => array1.filter((a) => !array2.some((b) => b[key] === a[key]));
+const getChangedSteps = (prevSteps, newSteps, compareBy) => {
+  return newSteps.filter((step) => {
+    const prevStep = prevSteps.find(({ cvs }) => step.cvs === cvs);
 
-module.exports.differenceBy = (array1, array2, key) => array1.filter((a) => !array2.some((b) => b[key] === a[key]));
+    if (!prevStep) {
+      return false;
+    }
+
+    const stepToCompare = pick(step, compareBy),
+      prevStepToCompare = pick(prevStep, compareBy);
+
+    return !isEqual(stepToCompare, prevStepToCompare);
+  });
+};
+
+module.exports = {
+  pick,
+  convertSpecToRecords,
+  differenceBy,
+  getChangedSteps,
+};
--- scripts/utils.spec.js
+++ scripts/utils.spec.js
@@ -1,6 +1,6 @@
-const { testSteps_v1 } = require('./mocks');
+const { testSteps_v1, testSteps_v2, testSteps_v2_changed } = require('./mocks');
 
-const { convertSpecToRecords, differenceBy } = require('./utils');
+const { convertSpecToRecords, differenceBy, getChangedSteps, pick } = require('./utils');
 
 describe('Script utils', () => {
   test('convertSpecToRecords', () => {
@@ -21,5 +21,46 @@ describe('Script utils', () => {
     );
 
     expect(diff).toEqual([{ a: 2, b: 'whatever2' }]);
+  });
+
+  test('pick', () => {
+    const obj = { a: 1, b: 2, c: { d: 3 }, e: [4, 5, 6] };
+
+    expect(pick(obj, [])).toEqual({});
+    expect(pick(obj, ['a', 'c'])).toEqual({ a: 1, c: { d: 3 } });
+    expect(pick(obj, ['c.d'])).toEqual({ 'c.d': 3 });
+    expect(pick(obj, ['e[1]'])).toEqual({ 'e[1]': 5 });
+
+    expect(pick(null, ['x'])).toEqual({});
+    expect(pick('abc', ['x'])).toEqual({});
+  });
+
+  describe('getChangedSteps', () => {
+    const { steps: stepsV1 } = convertSpecToRecords(testSteps_v1),
+      { steps: stepsV2 } = convertSpecToRecords(testSteps_v2),
+      { steps: stepsV2Changed } = convertSpecToRecords(testSteps_v2_changed);
+
+    const compareBy = ['info', 'step.asset_urls'];
+
+    test('when there are only new steps', () => {
+      const chnagedSteps = getChangedSteps([], stepsV1, compareBy);
+
+      expect(chnagedSteps).toHaveLength(0);
+    });
+
+    test('when previous steps are not changed', () => {
+      const chnagedSteps = getChangedSteps(stepsV1, stepsV2, compareBy);
+
+      expect(chnagedSteps).toHaveLength(0);
+    });
+
+    test('when previous steps are changed', () => {
+      const chnagedSteps = getChangedSteps(stepsV1, stepsV2Changed, compareBy);
+
+      expect(chnagedSteps).toHaveLength(1);
+
+      expect(chnagedSteps[0].info.maintainer).toBe('verified');
+      expect(chnagedSteps[0].step.asset_urls['icon.svg']).toBe('http://new-step-icon.url');
+    });
   });
 });
